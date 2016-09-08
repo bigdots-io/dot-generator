111a308fab99e6dca1f1cbc7b102022b25e230d2
Roy Kolak <roy.kolak@gmail.com>
2016-09-07 20:33:13 -0500

    support for right alignment and font json reorg

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
 "use strict";
 
 var Fonts = {
-  'system-micro': require('./fonts/system-micro/characters')
+  'system-micro': require('./fonts/system-micro'),
+  'system-medium': require('./fonts/system-medium')
 };
 
 class TypeWriter {
   constructor(options) {
     options = options || {};
-
     this.font = options.font;
     this.column = options.startingColumn || 0;
     this.row = options.startingRow || 0;
-    this.spaceBetweenLetters = options.spaceBetweenLetters || 2;
+    this.spaceBetweenLetters = options.spaceBetweenLetters || 1;
+    this.alignment = options.alignment || 'left';
   }
 
   text(copy, callback) {
-    var characters = Fonts[this.font];
-    for (let i = 0; i < copy.length; i++) {
-      var points = characters[copy[i]];
-      if(points) {
-        points.forEach((point) => {
-          callback({
-            y: this.row + point.y,
-            x: this.column + point.x
-          });
-        });
+    var font = Fonts[this.font],
+        characters = font.characters;
 
-        this.column = this.column + 4 + this.spaceBetweenLetters;
+    if(this.alignment === 'left') {
+      for (let i = 0; i < copy.length; i++) {
+        var character = characters[copy[i]],
+            coordinates = character.coordinates;
+
+        if(coordinates) {
+          coordinates.forEach((point) => {
+            callback({
+              y: this.row + point.y,
+              x: this.column + point.x
+            });
+          });
+
+          var width = character.width || font.width;
+          this.column = this.column + width + this.spaceBetweenLetters;
+        }
+      }
+    } else {
+      this.column -= characters[copy[copy.length - 1]].width || font.width;
+      for (let i = copy.length - 1; i >= 0; i--) {
+        var character = characters[copy[i]],
+            coordinates = character.coordinates;
+
+        if(coordinates) {
+          coordinates.forEach((point) => {
+            callback({
+              y: this.row + point.y,
+              x: this.column + point.x
+            });
+          });
+
+          var width = character.width || font.width;
+          this.column = this.column - width - this.spaceBetweenLetters;
+        }
       }
     }
   }
