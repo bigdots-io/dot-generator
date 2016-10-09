d8fc7fdb4c4be8295ea7ce926aa652f6cb5a6c4f
Roy Kolak <roy.kolak@gmail.com>
2016-10-09 10:26:33 -0500

    more character refactoring

--- index.js
+++ index.js
@@ -3,12 +3,10 @@
 var Message = require('./lib/message');
 
 class Typewriter {
-  constructor(options) {
-    options = options || {};
-    this.font = options.font;
-    this.column = options.startingColumn || 0;
-    this.row = options.startingRow || 0;
-    this.wrap = options.wrap || 'no-wrap';
+  constructor(options = {}) {
+    this.font = options.font || 'system-6';
+    this.startingColumn = options.startingColumn || 0;
+    this.startingRow = options.startingRow || 0;
     this.spaceBetweenLetters = options.spaceBetweenLetters || 1;
     this.alignment = options.alignment || 'left';
     this.hex = options.hex || '#FFFFFF';
@@ -16,10 +14,6 @@ class Typewriter {
     this.options = options;
 
     this.text = "";
-  }
-
-  static availableFonts() {
-    return Object.keys(Fonts);
   }
 
   getWidth(text) {
@@ -33,70 +27,26 @@ class Typewriter {
       spaceBetweenLetters: this.spaceBetweenLetters
     });
 
-    var coordinates = message.coordinates();
-
-    // this.column = this.getStartingColumn(text);
-    //
-    // for (let i = 0; i < text.length; i++) {
-    //   var character = new Character(text[i], font),
-    //       characterWidth = character.getWidth();
-    //
-    //   if(character.isRenderable() && (text[i] !== this.text[i])) {
-    //     var coordinates = character.getCoordinates();
-    //
-    //     if(coordinates) {
-    //       if(this.text && this.text[i]) {
-    //         var previousCharacter = new Character(this.text[i], font);
-    //
-    //         if(previousCharacter) {
-    //           var previousCoordinates = previousCharacter.getCoordinates();
-    //           coordinates = this.removePreviousCharacterCoordinate(coordinates, previousCoordinates);
-    //         }
-    //       }
-    //
-    //       var results = this.processCharacterCoordinates(coordinates, characterWidth);
-    //       var coordinatesOut = coordinatesOut.concat(results);
-    //     }
-    //   }
-    //
-    //   this.column += (characterWidth + this.spaceBetweenLetters);
-    // }
-    //
-    // this.text = text;
-
-    var startingColumn = this.options.startingColumn;
+    var startingColumn = this.startingColumn;
 
     if(this.alignment === 'right') {
       startingColumn -= message.getWidth();
     }
 
-    var transformedCoordinates = coordinates.map((coordinate) => {
-      return {
-        hex: this.hex,
-        x: coordinate.x + startingColumn,
-        y: coordinate.y
-      };
+    var coordinates = message.renderCoordinates({
+      previousText: this.text,
+      columnOffset: startingColumn,
+      rowOffset: this.startingRow
     });
+
+    var transformedCoordinates = coordinates.map((coordinate) => {
+      coordinate.hex = coordinate.hex || this.hex;
+      return coordinate;
+    });
+
+    this.text = text;
 
     callback(transformedCoordinates);
-  }
-
-  removePreviousCharacterCoordinate(coordinates, previousCoordinates) {
-    previousCoordinates.forEach(function(previousCoordinate) {
-      var index = coordinates.findIndex(function(coordinate) {
-        return (coordinate.y === previousCoordinate.y && coordinate.x === previousCoordinate.x);
-      });
-
-      if(index === -1) {
-        coordinates.push({
-          y: previousCoordinate.y,
-          x: previousCoordinate.x,
-          hex: '#000000'
-        })
-      }
-    });
-
-    return coordinates;
   }
 }
 
