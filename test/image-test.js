974285a08bbd67b79d1a7ca036511f0c6fb0dc58
Roy Kolak <roy.kolak@gmail.com>
2016-11-21 20:22:23 -0600

    comment back in image tests

--- test/image-test.js
+++ test/image-test.js
@@ -1,27 +1,27 @@
-// var assert = require('assert');
-// var DotGenerator = require('../index');
-//
-// describe('Generating dots from image', function() {
-//
-//   var dotGenerator = new DotGenerator();
-//
-//   describe('from url', function() {
-//     it('should render an image', function(done) {
-//       dotGenerator.image('./test/fixtures/green-ball.png', {
-//         onSuccess: (dots) => {
-//           assert.deepEqual(require('./fixtures/png'), dots)
-//           done();
-//         }
-//       });
-//     });
-//
-//     it('should render an animated image', function(done) {
-//       dotGenerator.image('./test/fixtures/icon-laugh.gif', {
-//         onSuccess: (dots) => {
-//           assert.deepEqual(require('./fixtures/animated-gif'), dots)
-//           done();
-//         }
-//       });
-//     });
-//   });
-// });
+var assert = require('assert');
+var DotGenerator = require('../index');
+
+describe('Generating dots from image', function() {
+
+  var dotGenerator = new DotGenerator();
+
+  describe('from url', function() {
+    it('should render an image', function(done) {
+      dotGenerator.image('./test/fixtures/green-ball.png', {
+        onSuccess: (dots) => {
+          assert.deepEqual(require('./fixtures/png'), dots)
+          done();
+        }
+      });
+    });
+
+    it('should render an animated image', function(done) {
+      dotGenerator.image('./test/fixtures/icon-laugh.gif', {
+        onSuccess: (dots) => {
+          assert.deepEqual(require('./fixtures/animated-gif'), dots)
+          done();
+        }
+      });
+    });
+  });
+});
