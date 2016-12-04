96e3831d19a7cca8b0c996a77b69bc8cb98553c9
Roy Kolak <roy.kolak@gmail.com>
2016-12-04 17:04:58 -0600

    solid color generator

--- index.js
+++ index.js
@@ -25,6 +25,24 @@ class DotGenerator {
   image(url, callbacks) {
     new ImageExploder(url).process(callbacks);
   }
+
+  color(color, dimensions) {
+    var height = dimensions.width,
+        width = dimensions.width,
+        out = [];
+
+    for(var y = 0; y < height; y++) {
+      for(var x = 0; x < width; x++) {
+        out.push({y: y, x: x, hex: color});
+      }
+    }
+
+    return {
+      width: dimensions.width,
+      height: dimensions.height,
+      dots: out
+    };
+  }
 }
 
 module.exports = DotGenerator;
